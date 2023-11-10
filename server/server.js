6bf1697a5ab8c843ee8c0afb70a965e668dbf3b8
Trishane <trishaneww@gmail.com>
2023-11-10 03:09:50 -0500

    backend post request validation

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const knex = require("knex")(require("./knexfile"));
 const app = express();
 const cors = require("cors");
+const validation = require("./utils/validation")
 require("dotenv").config();
 
 app.use(express.json());
@@ -32,23 +33,32 @@ app.get("/inventories", async (req, res) => {
 });
 
 app.post("/warehouses", async (req, res) => {
-  try {
-    console.log(req.body);
-    const data = await knex.insert(req.body).into("warehouses");
-    const newWarehouseId = data[0];
-    const createdWarehouse = await knex("warehouses").where({
-      id: newWarehouseId,
-    });
 
-    // res.status(201).json(createdWarehouse);
-    console.log(createdWarehouse);
-  } catch (err) {
-    res.status(500).send({ message: `Unable to create new warehouse: ${err}` });
-    console.log({ message: `Unable to create new warehouse: ${err}` });
+  if (!validation.isEmailValid(req.body.contact_email) || !validation.isPhoneValid(req.body.contact_phone) ||!req.body.address
+      ||!req.body.city || !req.body.country || !req.body.warehouse_name || !req.body.contact_name || !req.body.contact_position) {
+      res.send("Invalid Field")
+  } else {
+    try {
+      console.log(req.body);
+      const data = await knex.insert(req.body).into("warehouses");
+      const newWarehouseId = data[0];
+      const createdWarehouse = await knex("warehouses").where({
+        id: newWarehouseId,
+      });
+
+      console.log(createdWarehouse);
+    } catch (err) {
+      res.status(500).send({ message: `Unable to create new warehouse: ${err}` });
+      console.log({ message: `Unable to create new warehouse: ${err}` });
+    }
   }
 });
 
+
 app.post("/inventories", async (req, res) => {
+  if (!req.body.category ||!req.body.description || !req.body.item_name || !req.body.warehouse_quantity || !req.body.status || !req.body.warehouse_id){
+      res.send("Invalid Field")
+  } else {
   try {
     console.log(req.body);
     const data = await knex.insert(req.body).into("inventories");
@@ -57,12 +67,12 @@ app.post("/inventories", async (req, res) => {
       id: newInventoryId,
     });
 
-    // res.status(201).json(createdWarehouse);
     console.log(createdInventory);
   } catch (err) {
     res.status(500).send({ message: `Unable to create new Inventory: ${err}` });
     console.log({ message: `Unable to create new Inventory: ${err}` });
   }
+}
 });
 
 // Delete a warehouse/:id
@@ -108,6 +118,7 @@ app.delete("/inventories/:id", async (req, res) => {
     });
   }
 });
+
 
 app.listen(5050, () => {
   console.log(`running at http://localhost:5050`)
